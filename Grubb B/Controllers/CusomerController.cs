528b16b2c74c28759c7d0c37e3a8c9435a92f0dc
Rasmus Owen <Rasmus.Owen@hotmail.com>
2019-10-25 12:37:03 +0200

    Lagt till CustomerController

---
+++ Grubb B/Controllers/CusomerController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Npgsql;
+
+namespace Grubb_B.Controllers
+{
+    public class CustomerController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+
+
+            ViewBag.Message = "Hello World!";
+            
+            
+            
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
