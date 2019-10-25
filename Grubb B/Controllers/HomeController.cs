21445b3101ca3e723e4ff8bd79ee1a91d1294270
Petter edlund <peed1500@student.miun.se>
2019-10-25 11:25:28 +0200

    postgres

--- Grubb B/Controllers/HomeController.cs
+++ Grubb B/Controllers/HomeController.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Npgsql;
 
 namespace Grubb_B.Controllers
 {
     public class HomeController : Controller
     {
+        NpgsqlConnection conn = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["dbConn"].ConnectionString);
         public ActionResult Index()
         {
             return View();
@@ -15,7 +18,31 @@ namespace Grubb_B.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            try
+            {
+                using (var conn = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["dbConn"].ConnectionString))
+                {
+
+                    conn.Open();
+                    using (var cmd = new NpgsqlCommand())
+                    {
+                        cmd.Connection = conn;
+
+                        cmd.CommandText = "select * from freelancer;";
+
+                        cmd.ExecuteReader();
+                    }
+                    conn.Close();
+
+
+                }
+            }
+            catch (PostgresException ex)
+            {
+                ViewBag.Message = ex.Message;
+            }
+            
+            
 
             return View();
         }
